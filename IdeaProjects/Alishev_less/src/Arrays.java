bbd58d3e1d35d86fef21add19e59b8dc24f6500b
zimingv <kenza_z@mail.ru>
2019-04-19 14:51:43 +0800

    arrays1

--- IdeaProjects/Alishev_less/src/Arrays.java
+++ IdeaProjects/Alishev_less/src/Arrays.java
@@ -1,6 +1,16 @@
 public class Arrays {
     public static void main(String[] args) {
-         int number = 10;                 //[10]
-         int [] numbers = new int [5];    //numbers --> [массив]
+        int number = 10;                 //[10]
+        int[] numbers = new int[5];    //numbers --> [массив]
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 10;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        int[] numbers1 = {1, 2, 3};
+        for (int i = 0; i < numbers1.length; i++) {
+            System.out.println(numbers1[i]);
+        }
     }
 }
