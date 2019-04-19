c7ab1c5a8af2f7a0493426be2ff0d3f1ec3f4aa0
zimingv <kenza_z@mail.ru>
2019-04-19 14:21:45 +0800

    123

---
+++ IdeaProjects/Alishev_less/src/Arrays.java
@@ -0,0 +1,6 @@
+public class Arrays {
+    public static void main(String[] args) {
+         int number = 10;                 //[10]
+         int [] numbers = new int [5];    //numbers --> [массив]
+    }
+}
