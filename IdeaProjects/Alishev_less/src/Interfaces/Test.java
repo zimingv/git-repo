20a59f7d2ee3a9c9e8cc3b1c48726ec94352d752
zimingv <kenza_z@mail.ru>
2019-04-24 22:20:09 +0800

    24/04

---
+++ IdeaProjects/Alishev_less/src/Interfaces/Test.java
@@ -0,0 +1,21 @@
+package Interfaces;
+
+public class Test {
+    public static void main(String[] args) {
+//        Animal animal1 = new Animal(1);
+//        Person person1 = new Person("Bob");
+//        animal1.sleep();
+//        person1.sayHello();
+//        animal1.showInfo();
+//        person1.showInfo();
+        Info info1 = new Animal(1);
+        Info info2 = new Person("Bob");
+        info1.showInfo();
+        info2.showInfo();
+        outputInfo(info1);
+        outputInfo(info2);
+    }
+    public static void outputInfo(Info info) {
+        info.showInfo();
+    }
+}
