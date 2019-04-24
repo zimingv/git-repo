0d9c3ba213677b4e8fa31c1218a81f60decbfd3b
zimingv <kenza_z@mail.ru>
2019-04-25 00:34:17 +0800

    25/04 interfaces

--- IdeaProjects/Alishev_less/src/Interfaces/Info.java
+++ IdeaProjects/Alishev_less/src/Interfaces/Info.java
@@ -1,5 +1,5 @@
 package Interfaces;
 
 public interface Info {
-    public void showInfo();
+    public void showInfo();  // Если поставить default перед названием метода, то здесь можно прописывать его реализацию.
 }
--- IdeaProjects/Alishev_less/src/Interfaces/Test.java
+++ IdeaProjects/Alishev_less/src/Interfaces/Test.java
@@ -10,10 +10,14 @@ public class Test {
 //        person1.showInfo();
         Info info1 = new Animal(1);
         Info info2 = new Person("Bob");
-        info1.showInfo();
-        info2.showInfo();
-        outputInfo(info1);
-        outputInfo(info2);
+//        info1.showInfo();
+//        info2.showInfo();
+//        outputInfo(info1);
+//        outputInfo(info2);
+        Animal animal1 = new Animal(1);
+        Person person1 = new Person("Bob");
+        outputInfo(animal1);
+        outputInfo(person1);
     }
     public static void outputInfo(Info info) {
         info.showInfo();
