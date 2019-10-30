67dd39fecb8468a63d0c07e1376a13845c66f400
zimingv <kenza_z@mail.ru>
2019-10-30 22:27:04 +0800

    30/10

--- IdeaProjects/Alishev_less/src/Continue.java
+++ IdeaProjects/Alishev_less/src/Continue.java
@@ -1,10 +1,12 @@
 public class Continue {
     public static void main(String[] args) {
         for (int i = 0; i<=15; i++) {
-            if (i%2==0) {
+            if (i%2==0)
+            {
+                System.out.println("Это четное число " + i);
                 continue;
             }
-            System.out.println("Это нечетное число" + i);
+            System.out.println("Это нечетное число " + i);
         }
     }
 }
