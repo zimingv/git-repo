8b80233e58d1dc44033d1e001486314bcf2136d4
zimingv <kenza_z@mail.ru>
2019-11-20 21:30:51 +0800

    20.11/3

---
+++ IdeaProjects/Alishev_less/src/Кbln.java
@@ -0,0 +1,10 @@
+// Чтение символа с клавиатуры
+class Кbln {
+    public static void main(String args[])
+            throws java.io.IOException {
+        char ch;
+        System.out.print("Haжмитe нужную клавишу, а затем клавишу ENTER: ");
+        ch = (char) System.in.read(); // получить символ
+        System.out.println("Bы нажали клавишу " + ch);
+    }
+}
