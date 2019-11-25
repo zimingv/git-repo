87842fb3da0bbb78976ebb2b0dd812ded6467c59
zimingv <kenza_z@mail.ru>
2019-11-25 21:28:31 +0800

    25.11/1

---
+++ IdeaProjects/Alishev_less/src/Help2.java
@@ -0,0 +1,61 @@
+/*
+Упражнение 3.2
+Расширенная справочная система, в которой для обработки
+результатов выбора из меню используется цикл do-while
+*/
+class Help2 {
+    public static void main(String args[])
+            throws java.io.IOException {
+        char choice, ignore;
+
+        do {
+            System.out.println("Cпpaвкa:");
+            System.out.println(" 1. if");
+            System.out.println(" 2. switch");
+            System.out.println(" 3. for");
+            System.out.println(" 4. while");
+            System.out.println(" 5. do-while\n");
+            System.out.print("Bыбepитe: ");
+
+            choice = (char) System.in.read();
+
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+        } while (choice < '1' | choice > '5');
+
+        System.out.println("\n");
+
+        switch (choice) {
+            case '1':
+                System.out.println("Oпepaтop if:\n");
+                System.out.println("if(ycлoвиe) оператор;");
+                System.out.println("else оператор;");
+                break;
+            case '2':
+                System.out.println("Oпepaтop switch:\n");
+                System.out.println("switch(выpaжeниe) {");
+                System.out.println(" case константа:");
+                System.out.println(" последовательность операторов");
+                System.out.println(" break;");
+                System.out.println(" // ... ");
+                System.out.println("}");
+                break;
+            case '3':
+                System.out.println("Oпepaтop for:\n");
+                System.out.print("for(инициaлизaция; условие; итерация)");
+                System.out.println(" оператор;");
+                break;
+            case '4':
+                System.out.println("Oпepaтop while:\n");
+                System.out.println("while(ycлoвиe) оператор;");
+                break;
+            case '5':
+                System.out.println("Oпepaтop do-while:\n");
+                System.out.println("do {");
+                System.out.println(" оператор;");
+                System.out.println("} while (условие};");
+                break;
+        }
+    }
+}
