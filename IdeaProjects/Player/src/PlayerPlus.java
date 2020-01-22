cb844cc140a02ee74be22b9684fe26a32f7b441c
zimingv <kenza_z@mail.ru>
2020-01-22 23:29:44 +0800

    22/01

--- IdeaProjects/Player/src/PlayerPlus.java
+++ IdeaProjects/Player/src/PlayerPlus.java
@@ -4,11 +4,16 @@ public class PlayerPlus extends Player {
     private static int playerCount = 0;
     private static double totalOfAverages = .000;
     private static DecimalFormat decFormat = new DecimalFormat();
+    private static DecimalFormat decFormat2 = new DecimalFormat();
 
     static {
         decFormat.setMaximumIntegerDigits(0);
         decFormat.setMaximumFractionDigits(3);
         decFormat.setMinimumFractionDigits(3);
+
+        decFormat2.setMaximumIntegerDigits(1);
+        decFormat2.setMaximumFractionDigits(0);
+        decFormat2.setMinimumFractionDigits(0);
     }
 
     public PlayerPlus(String name, double average) {
@@ -23,5 +28,9 @@ public class PlayerPlus extends Player {
 
     public static String findTeamAverageString() {
         return decFormat.format(totalOfAverages / playerCount);
+    }
+
+    public static String countString() {
+        return decFormat2.format(playerCount);
     }
 }
--- IdeaProjects/Player/src/TeamFrame.java
+++ IdeaProjects/Player/src/TeamFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
@@ -11,19 +12,29 @@ public class TeamFrame extends JFrame {
         PlayerPlus player;
         Scanner hankeesData = new Scanner(new File("Hankees.txt"));
 
+        Font font = new Font("Verdana", Font.BOLD, 12);
+        add (new JLabel("№"));
+        add (new JLabel("  Имя игрока"));
+        add (new JLabel("Показатель игрока"));
+
+        add (new JLabel("--------------------"));
+        add (new JLabel("  --------------------"));
+        add (new JLabel("--------------------"));
+
         for (int num = 1; num <= 9; num++) {
             player = new PlayerPlus(hankeesData.nextLine(), hankeesData.nextDouble());
             hankeesData.nextLine();
+            add (new JLabel(PlayerPlus.countString()));
             addPlayerInfo(player);
         }
 
-        add (new JLabel());
-        add (new JLabel("  ------"));
-        add (new JLabel("Средний показатель команды:"));
+
+        add (new JLabel("--------------------"));
+        add (new JLabel("  Средний показатель команды:"));
         add (new JLabel(PlayerPlus.findTeamAverageString()));
 
         setTitle("Неведомая херня из бейсбола");
-        setLayout(new GridLayout(11,2,20,3));
+        setLayout(new GridLayout(13,3,20,3));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
