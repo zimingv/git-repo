c0230c78b4acdf305adce4ac86266c7d29683424
zimingv <kenza_z@mail.ru>
2020-02-13 22:50:05 +0800

    13.02

---
+++ IdeaProjects/GetGoing/src/EnglishSpeakingWorld.java
@@ -0,0 +1,12 @@
+public class EnglishSpeakingWorld {
+    String mars = "  красная планета";
+
+    void visitPennsylvania() {
+        System.out.println("Выполняется visitPA:");
+
+        String mars = "   родной город Джанин";
+
+        System.out.println(mars);
+        System.out.println(this.mars);
+    }
+}
---
+++ IdeaProjects/GetGoing/src/GetGoing.java
@@ -0,0 +1,9 @@
+public class GetGoing {
+    public static void main(String[] args) {
+        System.out.println("Выполняется main");
+        EnglishSpeakingWorld e = new EnglishSpeakingWorld();
+
+        System.out.println(e.mars);
+        e.visitPennsylvania();
+    }
+}
