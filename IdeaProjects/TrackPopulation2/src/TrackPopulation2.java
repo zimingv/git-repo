bf50a63cdd414330f8d7dfc4b62fd4962c861e10
zimingv <kenza_z@mail.ru>
2020-02-18 23:19:55 +0800

    18.02

---
+++ IdeaProjects/TrackPopulation2/src/TrackPopulation2.java
@@ -0,0 +1,12 @@
+public class TrackPopulation2 {
+    public static void main(String[] args) {
+        int smackoverARpop = 2232;
+
+        smackoverARpop = birth(smackoverARpop);
+        System.out.println(smackoverARpop);
+    }
+
+    static int birth(int cityPop) {
+        return cityPop+1;
+    }
+}
